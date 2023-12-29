2c2dde4797356a08db4c04691ae1724a251c5287
P1nki17 <106439049+P1nki17@users.noreply.github.com>
2023-12-29 23:46:42 +0300

    Add files via upload

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -3,7 +3,13 @@ document.addEventListener('click', function (event) {
     if (!event.target.hasAttribute('data-toggle-fullscreen')) return;
     // если элемент уже в полноэкранном режиме, выйти из него
     // В противном случае войти в полный экран
-    if (document.fullscreenElement) document.exitFullscreen();
-    else document.documentElement.requestFullscreen();
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+        document.querySelector(".btn__fullscreen").classList.remove("is-active");
+    } 
+    else {
+        document.documentElement.requestFullscreen();
+        document.querySelector(".btn__fullscreen").classList.add("is-active");
+    }
 
 }, false);
