020e67bf8c1d67ea14e1612c78f1ab727bc9c5d6
P1nki17 <106439049+P1nki17@users.noreply.github.com>
2023-12-30 02:41:14 +0300

    Update fullscreen.js

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -4,11 +4,11 @@ document.addEventListener('click', function (event) {
     // если элемент уже в полноэкранном режиме, выйти из него
     // В противном случае войти в полный экран
     if (document.fullscreenElement) {
-        document.exitFullscreen();
+        document.webkitExitFullscreen()
         document.querySelector(".btn__fullscreen").classList.remove("is-active");
     } 
     else {
-        document.documentElement.requestFullscreen();
+        document.documentElement.webkitRequestFullscreen();
         document.querySelector(".btn__fullscreen").classList.add("is-active");
     }
 
